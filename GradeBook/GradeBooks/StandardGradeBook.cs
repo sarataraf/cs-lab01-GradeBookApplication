04474aa1483c2a119bed760be1e9704222dfc958
sarataraf <rafalsarata@gmail.com>
2022-04-02 16:08:11 +0200

    Added StadardGradeBook, RankerdGradeBook and GetLetterGrade

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook
-    {
+    public class StandardGradeBook : BaseGradeBook {
+        public class StandardGradeBook(string name): base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
     }
 }
